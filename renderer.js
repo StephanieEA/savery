57341316e5a2f0abad55b4b3df700d30a4e4654b
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-03-28 14:13:05 -0600

    Initial append function works

--- renderer.js
+++ renderer.js
@@ -1,3 +1,15 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const $ = require('jquery');
+
+$('#save-recipe-btn').on('click', () => {
+  const title = $('#title-field').val();
+  const link = $('#url-field').val();
+  saveRecipe(title, link);
+})
+
+const saveRecipe = (title, link) => {
+  $('#recipe-box').append(title)
+}
