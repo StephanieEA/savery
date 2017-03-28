fa6a377aa42cc283d4223edde9c7ed97dbc987a7
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-03-28 14:33:45 -0600

    Adds check for http:// at beginning of link, link redirects

--- renderer.js
+++ renderer.js
@@ -7,9 +7,22 @@ const $ = require('jquery');
 $('#save-recipe-btn').on('click', () => {
   const title = $('#title-field').val();
   const link = $('#url-field').val();
-  saveRecipe(title, link);
+  const newLink = validateUrl(link);
+  saveRecipe(title, newLink);
 })
 
 const saveRecipe = (title, link) => {
-  $('#recipe-box').append(title)
+
+  $('#recipe-box').append(`<li>
+    <h2>${title}</h2>
+    <a href="${link}">link</a>
+  </li>`)
+}
+
+const validateUrl = (link) => {
+  const urlRegex = /^(http|https)?:\/\/[w]{2,4}[a-zA-Z0-9-\.]+\.[a-z]{1,10}/
+  if(!urlRegex.test(link)){
+    link = 'https://' + link
+  }
+  return link;
 }
